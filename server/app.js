30fc2f3bbe5a4eb238dbb40ca8319fa79090af50
Elias <elias0789@gmail.com>
2016-02-23 16:14:41 +0000

    Sort basic handlers

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var env2 = require('env2')("./config.env");
-var port = process.env.PORT || 8000;
 var router = require('./router.js');
+var port = process.env.PORT || 8000;
 
 http.createServer(router).listen(port, function(){
   console.log("listening on port "+port);
--- server/router.js
+++ server/router.js
@@ -4,19 +4,32 @@ var fs = require('fs');
 function router(req, res){
   var url = req.url;
   if(url === "/"){
-    fs.readFile(__dirname + "/../public/index.html", function(err, content){
-      if(err){
-        res.writeHead(500, {"Content-type": "text/html"});
-        console.log(err);
-        res.end(err);
-      } else {
-        res.writeHead(200, {"Content-type": "text/html"});
-        res.end(content.toString('utf8'));
-      }
-    });
-  } else if(url===""){
+    // index page
+    respondWithFile('/public/index.html', 'html', res)
+
+  } else if(url.indexOf('.') > -1 && url.search('/public') === 0){
+    // public resources
+    var ext = req.url.split('.')[1]
+    respondWithFile(req.url, ext, res)
+
+  } else {
+    // 404
+    respondWithFile('/public/404.html', 'html', res)
 
   }
+}
+
+function respondWithFile(path, ext, res) {
+  fs.readFile(__dirname + '/..' + path, function(error, content) {
+    if (error) {
+      console.log(error)
+      res.writeHead(500, {"Content-Type": "text/html"})
+      res.end(error.toString('utf8'))
+    } else {
+      res.writeHead(200, {"Content-Type": "text/" + ext})
+      res.end(content.toString('utf8'))
+    }
+  })
 }
 
 

e1ae2d882414aa9b8da12e2808266bce51496e67
Elias <elias0789@gmail.com>
2016-02-24 23:58:07 +0000

    alter tests to make use of environment variables.

--- tests/db.test.js
+++ tests/db.test.js
@@ -3,11 +3,14 @@ var redis = require("redis");
 var db = require('../server/db.js')
 var client, test = {};
 
+var dbUrl = process.env.REDIS_URL || 'redis://localhost:6379'
+var dbNum = process.env.REDIS_DB || 16
+
 test.module1 = tape({
   setup: function(t){
-    client = redis.createClient(process.env.REDIS_URL)
-    client.select(1, function(){
-      console.log("connected to database 1")
+    client = redis.createClient(dbUrl)
+    client.select(dbNum, function(){
+      console.log("connected to database number " + dbNum)
     })
     t.end()
   },
@@ -47,14 +50,12 @@ test.module1("test messages can be logged in the database", function(t) {
   var room = "cooking"
   var message = "hey man, what is up?"
   var time = new Date(Date.now()).toISOString()
-  console.log(time);
 
   db.addUser(client, username, room, function(){
     db.addMessage(client, username, room, message, time, function() {
       client.lrange('messages_in_' + room, 0, -1, function(error, reply) {
-        t.ok(! error, 'Assert that messages have been successfully added')
-        console.log(reply);
         reply = reply.map(JSON.parse)
+        t.ok(! error, 'Assert that messages have been successfully added')
         t.equal(reply.length, 1, "Assert only one message has been added")
         t.equal(reply[0].sender, username, "Assert correct user has sent the message")
         t.equal(reply[0].message, message, "Assert message has been stored correctly")
--- tests/socketevents.test.js
+++ tests/socketevents.test.js
@@ -1,34 +1,42 @@
+var env2 = require('env2')('./config.env')
 var http = require('http')
-var router = require('../server/router.js')
-var sck = require("../server/socketevents.js")
+var redis = require('redis')
 var ioClient = require('socket.io-client')
 var tape = require('wrapping-tape')
-var port = process.env.PORT || 8080
+var router = require('../server/router.js')
+var sck = require("../server/socketevents.js")
 
-var server = http.createServer(router)
+var port = process.env.PORT || 8000
+var dbUrl = process.env.REDIS_URL || 'redis://localhost:6379'
+var dbNum = process.env.REDIS_DB || 16
+
 var test = {}, socket0, socket1
+var server = http.createServer(router)
+var client = redis.createClient(dbUrl)
+client.select(dbNum)
 
+function createSocketClient() {
+  return ioClient.connect('http://localhost:' + port, {
+    // 'reconnection delay': 0,
+    // 'reopen delay': 0,
+    // 'force new connection': true
+  })
+}
 
 test.module1 = tape({
   setup: function(t){
     server.listen(port, function(){
       console.log("listening on port "+ port);
-      sck(server);
+      sck.init(server, client);
     });
-    socket0 = ioClient.connect('http://localhost:' + port, {
-      'reconnection delay': 0,
-      'reopen delay': 0,
-      'force new connection': true
-    })
-    socket1 = ioClient.connect('http://localhost:' + port, {
-      'reconnection delay': 0,
-      'reopen delay': 0,
-      'force new connection': true
-    })
+    socket0 = createSocketClient()
+    socket1 = createSocketClient()
     t.end()
   },
 
   teardown: function(t){
+    client.end()
+    sck.kill()
     server.close()
     t.end()
   }
@@ -42,25 +50,34 @@ test.module1("userJoin event adds user to db and responds with message history",
   }
 
   // Have user 0 join the room
-  socket0.emit('userJoin', JSON.stringify(emission))
+  socket0.emit('userJoin', emission)
 
   // Assert that user 1 gets the expected data back
   // Active users should return a list of all users in the current room
   socket0.on('activeUsers', function(response) {
-    response = JSON.parse(response)
-    console.log("response: ", response);
-    t.equal(response.username, emission.username, "Listen for username of joining user")
+    t.equal(response.newUser, emission.username, "Listen for username of joining user")
     t.equal(response.room, emission.room, "Listen for room of joining user")
+    t.deepEqual(response.activeUsers, [emission.username], "Assert for list of active users")
     t.end()
   })
 })
 
 test.module1("test that messages are stored in the database", function(t){
-  var messageObj = {sender: "bugs-bunny", body:"test message", time: new Date(Date.now()).toISOString()}
-  var messageFromServer = "Message stored"
-  socket0.emit("")
-  socket0.emit("sendChat", JSON.stringify(messageObj))
-  socket0.on("updateChat", function(response){
-    t.equal(response, messageFromServer, "Listen for confirmation that the message has been stored")
+  // Join room 1
+  socket0.emit('userJoin', {username: 'sylvester', room: 'room1'})
+  socket1.emit('userJoin', {username: 'tweety', room: 'room1'})
+
+  var time = new Date(Date.now()).toISOString();
+
+  var messageObj = {sender: 'sylvester', body: 'test message', time: time}
+  var messageFromServer = {success: true, message: "Message stored"};
+  socket0.emit("sendChat", messageObj)
+  socket0.on("updateChat", function(response) {
+    t.deepEqual(response, messageFromServer, "Listen for confirmation that the message has been stored")
   })
+  socket1.on('updateChat', function(response) {
+    t.deepEqual(response, messageObj)
+  })
+
+  t.plan(2)
 })

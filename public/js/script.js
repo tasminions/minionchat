ac9517e03f0bff69d72902b29f03ec3a182baa93
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-02-24 15:47:11 +0000

    started user is tyopinh funcyion and apdate chat and send chat

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
 var socket = io.connect('http://localhost:8000');
+
 socket.on('connect',function(){
   var nickname = document.URL.split('nickname=')[1];
   console.log(nickname);
   socket.emit('userJoin',nickname);
+  socket.on('userJoin', function(nickname){
+    appendItemToList("allMessages").innerHTML = nickname + " has joined the PARTAY";
+  });
+
   socket.on('msgHistory',function( msgHistory ){
     var ulList = document.getElementById('allMessages');
     ulList.innerHTML = "";
@@ -14,16 +19,68 @@ socket.on('connect',function(){
       var msgTime    = messageObj.time;
       liNode.innerHTML = "Time: " + msgTime + " - " + originator + " said: " + msgBody;
       ulList.appendChild(liNode);
-    });   
+    });
   });
+
   socket.on('activeUsers',function(activeUsers){
     var ulList = document.getElementById('activeUsers');
     ulList.innerHTML = "";
     var activeUsersArr = JSON.parse( activeUsers );
     activeUsersArr.forEach(function(activeUser){
-      var liNode = document.createElement('li');
-      liNode.innerHTML = activeUser;
-      ulList.appendChild(liNode);
+      appendItemToList("activeUsers").innerHTML = activeUser;
     });
   });
+
+
+
+  document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    var messageObj = createMessageObj(nickname);
+    appendItemToList("allMessages").innerHTML = "Time: " + messageObj.time + " - " + messageObj.originator + " said: " + messageObj.body;
+    var chatObj = JSON.stringify(messageObj);
+    socket.emit('sendChat', chatObj);
+  });
+  socket.on("updateChat", function(message){
+
+
+  });
+// var typing = false;
+// var timeout = undefined;
+//
+// function timeoutFunction(){
+//   typing = false;
+//   socket.emit(noLongerTypingMessage);
+// }
+//
+// function onKeyDownNotEnter(){
+//   if(typing == false) {
+//     typing = true
+//     socket.emit(typingMessage);
+//     timeout = setTimeout(timeoutFunction, 5000);
+//   } else {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(timeoutFunction, 5000);
+//   }
+//
+// }
+  document.getElementById('sendChat').addEventListener('keyup', function(e){
+      socket.emit("userIsTyping", nickname);
+
+
+    })
 });
+
+
+function appendItemToList(list){
+  var ulList = document.getElementById(list);
+  var liNode = document.createElement('li');
+  ulList.appendChild(liNode);
+  return liNode;
+}
+function createMessageObj(nickname){
+  return {
+    "originator": nickname,
+    "body": document.getElementById('sendChat').value,
+    "time": Date.now()
+  };
+}

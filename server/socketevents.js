1fdb1abe084c251ca1b7b934814475becd7cce8b
franzmoro <franzmoro@hotmail.com>
2016-02-24 14:10:47 +0000

    socket events new user, msg history, activeuser frontend, no databasecall

--- server/socketevents.js
+++ server/socketevents.js
@@ -25,7 +25,27 @@ module.exports = function(server) {
   var sckt = require('socket.io')(server);
   sckt.sockets.on("connection", function(socket){
     socket.on('userJoin', function(nickname){
-    console.log(nickname);
+      console.log(nickname);
+      var testMsgHist = JSON.stringify([
+            {
+                "originator":"bugs",
+                "body":"here is a message",
+                "time":"24-01-2016:12.123321321312"
+            },
+            {
+              "originator":"daffy",
+              "body":"here is a message2",
+              "time":"24-01-2016:12.123321321312"
+            },
+            {
+              "originator":"me",
+              "body":"here is not a message",
+              "time":"24-01-2016:12.123321321312"
+            }
+        ]);
+      var activeUsers = JSON.stringify(["dafyy","bugs","popeye"]);
+      socket.emit('msgHistory',testMsgHist);
+      socket.emit('activeUsers',activeUsers)
     });
   });
 };

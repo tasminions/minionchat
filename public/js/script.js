c61d2d93ea6fbf39174a08360207049905aa4dd9
franzmoro <franzmoro@hotmail.com>
2016-02-24 16:39:13 +0000

    new chat urls (work) and user is typing (need backend to check if it works).

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
 var socket = io.connect('http://localhost:8000');
 
 socket.on('connect',function(){
-  var nickname = document.URL.split('nickname=')[1];
+  var urlParams = document.URL.split('/');
+  var nickname = urlParams[urlParams.length-1].replace('?nickname=','');
+  var room = urlParams[urlParams.length-2];
+  console.log(room)
   console.log(nickname);
+  var typing = false;
+  var connectionInfo = {nickname:nickname, room:room};
   socket.emit('userJoin',nickname);
   socket.on('userJoin', function(nickname){
     appendItemToList("allMessages").innerHTML = nickname + " has joined the PARTAY";
@@ -13,37 +18,74 @@ socket.on('connect',function(){
     ulList.innerHTML = "";
     var msgHistoryArr = JSON.parse( msgHistory );
     msgHistoryArr.forEach( function( messageObj ){
-      var liNode = document.createElement('li');
-      var originator = messageObj.originator;
-      var msgBody    = messageObj.body;
-      var msgTime    = messageObj.time;
-      liNode.innerHTML = "Time: " + msgTime + " - " + originator + " said: " + msgBody;
-      ulList.appendChild(liNode);
+      appendItemToList('allMessages').innerHTML = "Time: " + messageObj.time + " - " + messageObj.originator + " said: " + messageObj.body;
     });
   });
 
   socket.on('activeUsers',function(activeUsers){
+    // reset the active users list
     var ulList = document.getElementById('activeUsers');
     ulList.innerHTML = "";
+    // repopulate the active users list with all active users
     var activeUsersArr = JSON.parse( activeUsers );
     activeUsersArr.forEach(function(activeUser){
-      appendItemToList("activeUsers").innerHTML = activeUser;
+      var liNode = appendItemToList("activeUsers");
+      var aNode = document.createElement('a');
+      aNode.href = newChatUrl(nickname,activeUser);
+      aNode.target = '_blank';
+      liNode.appendChild(aNode);
+      aNode.innerHTML = activeUser;
+      aNode.id = activeUser;
     });
   });
-
-
 
   document.querySelector('form').addEventListener('submit', function(e){
     e.preventDefault();
     var messageObj = createMessageObj(nickname);
     appendItemToList("allMessages").innerHTML = "Time: " + messageObj.time + " - " + messageObj.originator + " said: " + messageObj.body;
+
     var chatObj = JSON.stringify(messageObj);
     socket.emit('sendChat', chatObj);
   });
   socket.on("updateChat", function(message){
-
+    // will update everyone's chat
 
   });
+
+  // "TYPING EVENT" EMITTERS AND LISTENERS
+  document.getElementById('sendChat').addEventListener('input', function(){
+    var userTypingObj = nickname;
+    socket.emit( "userIsTyping", userTypingObj );
+  });
+  socket.on('userIsTyping',function( userTyping ){
+    typing = true;
+    document.getElementById( userTyping ).innerHTML += ' is typing...';
+    setTimeout(function(){
+      document.getElementById( userTyping ).innerHTML -= ' is typing...';
+    },3000);
+  });
+
+});
+
+
+function appendItemToList(list){
+  var ulList = document.getElementById(list);
+  var liNode = document.createElement('li');
+  ulList.appendChild(liNode);
+  return liNode;
+}
+function createMessageObj(nickname){
+  return {
+    "originator": nickname,
+    "body": document.getElementById('sendChat').value,
+    "time": Date.now()
+  };
+}
+// create a two-way chat url based on alphabetical order
+function newChatUrl(currUser,otherUser){
+  var urlPart1 = currUser < otherUser ? '/'+currUser+'&'+otherUser : '/'+otherUser+'&'+currUser;
+  return urlPart1+'/?nickname='+currUser;
+}
 // var typing = false;
 // var timeout = undefined;
 //
@@ -63,24 +105,3 @@ socket.on('connect',function(){
 //   }
 //
 // }
-  document.getElementById('sendChat').addEventListener('keyup', function(e){
-      socket.emit("userIsTyping", nickname);
-
-
-    })
-});
-
-
-function appendItemToList(list){
-  var ulList = document.getElementById(list);
-  var liNode = document.createElement('li');
-  ulList.appendChild(liNode);
-  return liNode;
-}
-function createMessageObj(nickname){
-  return {
-    "originator": nickname,
-    "body": document.getElementById('sendChat').value,
-    "time": Date.now()
-  };
-}

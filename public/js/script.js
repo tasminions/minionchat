1ae5e419e31b640580c899db6f2454c662c0e4b5
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-02-24 11:19:09 +0000

    index html requests the chat.html page

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
 //
+// when you add your user name
+document.querySelector('form').addEventListener('submit', function(e){
+  e.preventDefault();
+  var nickname = document.getElementById('nickname').value;
+  var req = new XMLHttpRequest();
+  var url = "/main/"+nickname;
+  console.log(url);
+  req.onreadystatechange = function(){
+    if (req.statusCode === 200 && req.readystate === 400){
+      console.log("hey hey");
+
+    }
+  };
+  req.open("GET", url);
+  req.send();
+});
--- server/router.js
+++ server/router.js
@@ -3,33 +3,39 @@ var fs = require('fs');
 
 function router(req, res){
   var url = req.url;
+  console.log("urlsss" , url)
   if(url === "/"){
     // index page
-    respondWithFile('/public/index.html', 'html', res)
+    respondWithFile('/public/index.html', 'html', res);
+    console.log("jsjsjsjs");
 
   } else if(url.indexOf('.') > -1 && url.search('/public') === 0){
     // public resources
-    var ext = req.url.split('.')[1]
-    respondWithFile(req.url, ext, res)
+    var ext = req.url.split('.')[1];
+    respondWithFile(req.url, ext, res);
+    console.log("ksjdbfajfbksdfhb");
 
-  } else {
+  } else if(url.indexOf("/main/")>-1){
+    console.log("you are on the mainr oom");
+  }
+    else {
     // 404
-    respondWithFile('/public/404.html', 'html', res)
-
+    respondWithFile('/public/404.html', 'html', res);
+    console.log("hehehehehe");
   }
 }
 
 function respondWithFile(path, ext, res) {
   fs.readFile(__dirname + '/..' + path, function(error, content) {
     if (error) {
-      console.log(error)
-      res.writeHead(500, {"Content-Type": "text/html"})
-      res.end(error.toString('utf8'))
+      console.log(error);
+      res.writeHead(500, {"Content-Type": "text/html"});
+      res.end(error.toString('utf8'));
     } else {
-      res.writeHead(200, {"Content-Type": "text/" + ext})
-      res.end(content.toString('utf8'))
+      res.writeHead(200, {"Content-Type": "text/" + ext});
+      res.end(content.toString('utf8'));
     }
-  })
+  });
 }
 
 

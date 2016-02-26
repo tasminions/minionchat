337a73f0cf0d5435cebc058ca2d364eef84927e5
tas12 <tsultanah89gmail.comgit config --global user.name tasnimgit config --global user.email tsultanah89gmail.com>
2016-02-26 11:46:27 +0000

    added minion emojis

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 var socket = io.connect();
+var icon;
 
 socket.on('connect',function(){
   var urlParams = document.URL.split('/');
@@ -56,6 +57,19 @@ socket.on('connect',function(){
     }
   });
 
+  function addIcon(e){
+    icon = e.target.outerHTML
+    var messageObj = createMessageObj(username, room);
+    newMessage(messageObj);
+    var chatObj = messageObj;
+    socket.emit('sendChat', chatObj);
+  }
+
+  var iconNames = document.getElementsByClassName('icon')
+  for(i = 0; i < iconNames.length; i++) {
+    iconNames[i].addEventListener("click", addIcon)
+  }
+
   document.querySelector('form').addEventListener('submit', function(e){
     e.preventDefault();
     var messageObj = createMessageObj(username, room);
@@ -107,7 +121,7 @@ function appendItemToList(list){
 function createMessageObj(username, room){
   return {
     "sender": username,
-    "message": document.getElementById('sendChat').value,
+    "message": document.getElementById('sendChat').value || icon,
     "time": formatTime(Date.now()),
     "room": room
   }; //
